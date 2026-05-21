c7218905a51990d5fbb0945f288ddf6e58f8d9ad
mcun0005-fit2179 <mcun0005@student.monash.edu>
2026-05-21 10:33:36 +1000

    Add files via upload

---
+++ FIT2179-Assignment-2.js
@@ -0,0 +1,44 @@
+var vg_1 = "Unemployment-Rate-in-Australia-Chart.vega";
+vegaEmbed("#vis", vg_1).then(function(result) {
+
+}).catch(console.error);
+
+var vg_2 = "Youth-Unemployment-Rate-in-Australia-Chart.vega";
+vegaEmbed("#vis2", vg_2).then(function(result) {
+
+}).catch(console.error);
+
+var vg_3 = "Female-Unemployment-Rate-in-Australia-Chart.vega";
+vegaEmbed("#vis3", vg_3).then(function(result) {
+
+}).catch(console.error);
+
+var vg_4 = "Labor-Force-Participation-Rate-in-Australia-Chart.vega";
+vegaEmbed("#vis4", vg_4).then(function(result) {
+
+}).catch(console.error);
+
+var vg_5 = "Employment-Rate-in-Australia-Chart.vega";
+vegaEmbed("#vis5", vg_5).then(function(result) {
+
+}).catch(console.error);
+
+var vg_6 = "Employment-Type-in-Australia-Chart.vega";
+vegaEmbed("#vis6", vg_6).then(function(result) {
+
+}).catch(console.error);
+
+var vg_7 = "Employment-Type-in-Australia-Pie-Chart00.vega";
+vegaEmbed("#vis7", vg_7).then(function(result) {
+
+}).catch(console.error);
+
+var vg_8 = "Employment-Type-in-Australia-Pie-Chart25.vega";
+vegaEmbed("#vis8", vg_8).then(function(result) {
+
+}).catch(console.error);
+
+var vg_9 = "Manufacturing-Share-in-Australia-Chart.vega";
+vegaEmbed("#vis9", vg_9).then(function(result) {
+
+}).catch(console.error);
